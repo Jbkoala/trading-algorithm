e32653651ce770ceb7e0af9000f9195be06b305c
Jbkoala <jbailey0233@gmail.com>
2024-08-23 23:14:49 +0100

    Have compiled the project, which  I belive generated the files associated with 'SBE' FIX protocol. I have also made notes for Logger class.

--- algo-coding-exercise/getting-started/src/main/java/codingblackfemales/gettingstarted/MyAlgoLogic.java
+++ algo-coding-exercise/getting-started/src/main/java/codingblackfemales/gettingstarted/MyAlgoLogic.java
@@ -10,13 +10,15 @@ import org.slf4j.LoggerFactory;
 
 public class MyAlgoLogic implements AlgoLogic {
 
+    // Create an instance of logger
     private static final Logger logger = LoggerFactory.getLogger(MyAlgoLogic.class);
 
+    // I assume this is the concrete class SimpleAlgoStateImpl that object is being made from.
     @Override
     public Action evaluate(SimpleAlgoState state) {
 
         var orderBookAsString = Util.orderBookToString(state);
-
+    // info method stating: Normal but not too verbose
         logger.info("[MYALGO] The state of the order book is:\n" + orderBookAsString);
 
         /********
@@ -28,3 +30,18 @@ public class MyAlgoLogic implements AlgoLogic {
         return NoAction.NoAction;
     }
 }
+
+/*
+### slf4J - Simple Logging Facade for Java ###
+What is it? set of Interface and Abstractions that aim to consolidate and standardize logging in the JVM environment, while allowing decoupling
+Why use it? : Used to debug issues relating to class loading when handling external libraries
+1) Dependency for slf4J API pre-existed with forked repo
+2) Need to create an instance of the logger : LN14
+
+### Benefit of Logging ###
+- Logging has the concept of levels : OFF, FATAL, ERROR, WARN, INFO (shown in file), DEBUG, TRACE, ALL
+- Can send output to multiple places compared to System.out.println()
+ */
+
+
+
